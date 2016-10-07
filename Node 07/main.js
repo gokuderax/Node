b4bf6bff8fa1a4d1d2141eda3dcba786849bdfba
gokuderax <gokudera1992@gmail.com>
2016-10-07 16:14:22 +0200

    Actualizado node 07

--- Node 07/main.js
+++ Node 07/main.js
@@ -1,13 +1,21 @@
-var events = require('events');
-var eventEmitter = new events.EventEmitter();
+var events = require("events");var eventEmitter = new events.EventEmitter();
 
+var listner1 = function listner1(){
 
-var listner1= function listner1(){
-	console.log('listner1 executed.');
+    console.log("listner1 executed");
+
+}
+var listner2 = function listner2(){
+
+    console.log("listner2 executed");
+
 }
 
-var listner2= function listner2(){
-	console.log('listner2 executed.');
-}
+eventEmitter.addListener("connection", listner1);
+eventEmitter.addListener("connection", listner2);var eventListeners = require("events").EventEmitter.listenerCount(eventEmitter, "connection");console.log(eventListeners + " Listner(s) listening to connection event.");
+eventEmitter.emit("connection");
 
-eventEmitter.addListener('connection' listner1);
+eventEmitter.removeListener("connection", listner1);
+
+console.log("Listner1 will not listen now.");
+eventEmitter.emit("connection");eventListeners = require("events").EventEmitter.listenerCount(eventEmitter, "connection");console.log(eventListeners + " Listner(s) listner to connection event");console.log("Program Ended.");
