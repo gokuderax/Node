197b297409bbca6d1fb1ea94951eb526a252e5d0
gokuderax <gokudera1992@gmail.com>
2016-11-18 17:02:30 +0100

    actualizado node 16

--- Node 16/app.js
+++ Node 16/app.js
@@ -5,6 +5,12 @@ app.get('/', function (req,res) {
 	res.send('Hello World!');
 });
 
+app.get('/name/:user_name', function(req,res){
+	res.status(200);
+	res.set('Content-type', 'text/html');
+	res.send('<html> <body>'+'<h1> Hello '+req.params.user_name + '</h1>'+'</body></html>');
+});
+
 app.listen(3000, function(){
 	console.log('Example app listening on port 3000!');
 });
