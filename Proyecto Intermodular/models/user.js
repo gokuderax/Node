a3aa1d346b4621164937a9fec051dd99082366a0
gokudera <gokudera1992@gmail.com>
2017-02-01 21:37:04 +0100

    Actualizado proyecto con user

--- Proyecto Intermodular/models/user.js
+++ Proyecto Intermodular/models/user.js
@@ -2,53 +2,34 @@
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const UserSchema = Schema({
-
-   cif: String,
-
-   nombre: String,
-
-   apelido1: String,
-
-   apellido2: String,
-
-   tipo_via: String,
-
-   nombre_via: String,
-
-   numero: String,
-
-   piso: String,
-
-   puerta: String,
-
-   cod_postal: String,
-
-   muncipio: String,
-
-   provincia: String,
-
-   pais: String,
-
-   telefono1: String,
-
-   telefono2: String,
-
-   email: String,
-
-   persona_contacto: String,
-
-   tipo: Number,
-
-   observaciones: String,
-
-   fecha_inicio: Date,
-
-   fecha_final: Date,
-
-   tienda: Number,
-
-   cargo: Number
+const UserSchema =new Schema({
+   typeUser:[],
+   nif:{type:String},
+   cif: {type:String},
+   nombre: {type:String},
+   apellido1: {type:String},
+   apellido2: {type:String},
+   tipo_via:{type:String},
+   nombre_via: {type:String},
+   numero: {type:Number},
+   piso: {type:String},
+   puerta: {type:String},
+   cod_postal:{type:String},
+   muncipio: {type:String},
+   provincia: {type:String},
+   pais: {type:String},
+   telefono1: {type:String},
+   telefono2: {type:String},
+   fax: {type:String},
+   email: {type:String,lowercase:true},
+   persona_contacto:{type:String,lowercase:true},
+   tipo_cliente:{type:String,lowercase:true},
+   password:{type:String,select:false},
+   fecha_inicio: {type:Date,default:Date.now()},
+   fecha_fin:  {type:Date,default:Date.now()},
+   cargo:{type:String},
+   lastLogin:{type:Date},
+   observaciones: {type:String},
 
 });
 
